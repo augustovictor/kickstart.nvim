70c444b61c49958a2f993a94a2c81f60f83e9350
victoraugusto <victor.costa@getsafari.com>
2025-11-22 20:43:31 -0300

    chore: Update plugins

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -193,6 +193,13 @@ vim.keymap.set('n', '<S-R>', ':e<CR>')
 vim.keymap.set('n', '<localleader>yp', function()
   vim.fn.setreg('+', vim.fn.expand '%:p:h')
 end, { desc = 'Copy file path' })
+
+-- Search
+vim.keymap.set('x', '/', '<Esc>/\\%V')
+
+-- Custom configs
+-- vim.opt.termguicolors = true
+-- vim.o.showtabline = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -456,7 +463,17 @@ require('lazy').setup({
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>sf', function()
+        require('telescope.builtin').find_files {
+          find_command = {
+            'rg',
+            '--files',
+            '--hidden',
+            '-g',
+            '!.git',
+          },
+        }
+      end, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
@@ -914,11 +931,85 @@ require('lazy').setup({
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
+        style = 'night',
         styles = {
-          comments = { italic = false }, -- Disable italics in comments
+          -- Your legacy config had almost no italics, so we disable them here
+          comments = { italic = false },
+          keywords = { italic = false },
+          functions = { italic = false },
+          variables = { italic = false },
+          sidebars = 'normal',
+          floats = 'normal',
         },
-      }
+        on_colors = function(colors)
+          -- 1. Base Palette
+          colors.bg = '#2b2b2b' -- Editor Background
+          colors.fg = '#a9b7c6' -- Main Text
+          colors.bg_dark = '#2b2b2b'
 
+          -- 2. Sidebar Specific (IntelliJ Classic Tool Window Grey)
+          colors.bg_sidebar = '#313335'
+          -- colors.bg_float = '#3c3f41' -- Slightly lighter for documentation popups
+
+          -- 3. Syntax Colors
+          colors.orange = '#CC7832'
+          colors.green = '#6A8755'
+          colors.yellow = '#FBC862'
+          colors.purple = '#9676ac'
+          colors.blue = '#6897BB'
+          colors.comment = '#8e9292'
+        end,
+
+        on_highlights = function(hl, c)
+          -- Editor Overrides
+          hl.Normal = { bg = c.bg, fg = c.fg }
+          hl.CursorLine = { bg = '#323232' }
+          hl.Visual = { bg = '#214283' }
+          hl.LineNr = { fg = '#888888', bg = '#323232' } -- Matches the gutter to the sidebar tone
+          hl.SignColumn = { bg = '#323232' }
+
+          -- Syntax Overrides
+          hl['@keyword'] = { fg = c.orange }
+          hl['@function'] = { fg = c.yellow }
+          hl['@string'] = { fg = c.green }
+          hl['@comment'] = { fg = c.comment }
+          hl['@variable'] = { fg = c.fg }
+          hl['@constant'] = { fg = c.purple }
+          hl['@property'] = { fg = c.purple }
+          hl['@type'] = { fg = c.purple }
+
+          ---------------------------------------------------------
+          -- SIDEBAR CONFIGURATION (IntelliJ Style)
+          ---------------------------------------------------------
+
+          -- 1. Backgrounds (Forces the gray tool-window look)
+          hl.NeoTreeNormal = { bg = c.bg_sidebar, fg = c.fg }
+          hl.NeoTreeNormalNC = { bg = c.bg_sidebar, fg = c.fg }
+          hl.NvimTreeNormal = { bg = c.bg_sidebar, fg = c.fg }
+
+          -- 2. Folders & Files
+          hl.NeoTreeRootName = { fg = c.fg, bold = true }
+          hl.NeoTreeDirectoryName = { fg = c.fg } -- IntelliJ folders are standard grey
+          hl.NeoTreeDirectoryIcon = { fg = c.fg }
+
+          -- 3. Active File Highlighting
+          -- IntelliJ highlights the currently open file with a different background or bold text
+          hl.NeoTreeFileNameOpened = { fg = c.fg, bold = true }
+          -- If you want the background highlight style:
+          -- hl.NeoTreeCursorLine = { bg = '#0d293e' }
+
+          -- 4. Git Status Colors (Matching IntelliJ)
+          hl.NeoTreeGitAdded = { fg = c.green }
+          hl.NeoTreeGitConflict = { fg = c.orange }
+          hl.NeoTreeGitDeleted = { fg = c.comment }
+          hl.NeoTreeGitIgnored = { fg = c.comment }
+          hl.NeoTreeGitModified = { fg = c.blue } -- Modified files are Blue in IntelliJ
+          hl.NeoTreeGitUntracked = { fg = c.orange }
+
+          -- 5. Window Separator
+          hl.WinSeparator = { fg = '#555555', bg = c.bg_sidebar }
+        end,
+      }
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
--- lua/custom/plugins/buffer_tabs.lua
+++ lua/custom/plugins/buffer_tabs.lua
@@ -1,12 +1,20 @@
 return {
-  'romgrk/barbar.nvim',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons', -- Required for icons
-  },
-  init = function()
-    vim.g.barbar_auto_setup = false
+  'akinsho/bufferline.nvim',
+  version = '*',
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function()
+    vim.opt.termguicolors = true
+    vim.o.showtabline = 2
+
+    require('bufferline').setup {
+      options = {
+        mode = 'buffers', -- or "tabs"
+        -- separator_style = 'slant', -- closest to IntelliJ's tab curvature
+        show_buffer_close_icons = false, -- IntelliJ hides close icons until hover
+        show_close_icon = false,
+        diagnostics = 'nvim_lsp',
+        always_show_bufferline = true,
+      },
+    }
   end,
-  opts = {
-    -- Customize options here if needed
-  },
 }
