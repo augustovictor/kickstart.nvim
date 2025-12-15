1e5ec8b453d11c992b7f79ba1622ffe9ef448bd6
victoraugusto <victor.costa@getsafari.com>
2025-12-14 22:18:30 -0300

    fix: Fix multi-cursor keymap conflict

--- lua/custom/plugins/vim-visual-multi.lua
+++ lua/custom/plugins/vim-visual-multi.lua
@@ -5,10 +5,10 @@ return {
     vim.g.VM_default_mappings = 0
     vim.g.VM_maps = {
       ['Find Under'] = '<C-d>',
-      ['Find Subword Under'] = '<C-d>',
+      -- ['Find Subword Under'] = '<C-d>',
       ['Select All'] = '<C-a>',
-      ['Add Cursor Down'] = '<C-j>',
-      ['Add Cursor Up'] = '<C-k>',
+      ['Add Cursor Down'] = '<C-S-j>',
+      ['Add Cursor Up'] = '<C-S-k>',
     }
     vim.g.VM_add_cursor_at_pos_no_mappings = 1
   end,
