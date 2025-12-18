a5d56e93454cced56b23be102491c776bd3e354a
victoraugusto <victor.costa@getsafari.com>
2025-12-18 08:09:14 -0300

    chore: Configure vim-visual-multi for selecting all

--- lua/custom/plugins/vim-visual-multi.lua
+++ lua/custom/plugins/vim-visual-multi.lua
@@ -5,8 +5,9 @@ return {
     vim.g.VM_default_mappings = 0
     vim.g.VM_maps = {
       ['Find Under'] = '<C-d>',
-      -- ['Find Subword Under'] = '<C-d>',
+      ['Find Subword Under'] = '<C-d>',
       ['Select All'] = '<C-a>',
+      ['Visual All'] = '<C-a>', -- Select all occurrences of visual selection
       ['Add Cursor Down'] = '<C-S-j>',
       ['Add Cursor Up'] = '<C-S-k>',
     }
